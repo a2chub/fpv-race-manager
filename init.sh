e1bae626d220855c4c380fd7cdd9463222aeed03
a2c <a2cgle@gmail.com>
2020-12-07 19:43:05 +0900

    1st ci

---
+++ init.sh
@@ -0,0 +1,8 @@
+
+
+# 前提条件
+# OSX 10.15.7 で動作確認済み
+# Google Drive Streamで、Home配下にsyncしていること
+# GoogleDriveのマイドライブにFDFフォルダーが登録されていること
+
+ln -s ~/Google\ Drive\ File\ Stream/マイドライブ/FDF_\(船橋ドロー ンフィールド\)/T-CUP/Vol1/data_csv ./data/data_csv
