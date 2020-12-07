b71ae52e516958e66c9720dca3a0d57ea9307cae
a2c <a2cgle@gmail.com>
2020-12-08 01:39:57 +0900

    json読み込み追加

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import json
 
 DATA_PATH = '../data/data_csv/'
 DATA_lap_file = 'Latest-Result.csv'
-DATA_result_file = 'result_A.csv'
+DATA_result_file = 'data.json'
 
 LAP_DATA = os.path.join(DATA_PATH, DATA_lap_file)
 RESULT_DATA = os.path.join(DATA_PATH, DATA_result_file)
@@ -19,23 +19,12 @@ with open(LAP_DATA, newline='') as csvfile:
     _tmp = ', '.join(row).split(',')
     _pilot = {_tmp[0]: _tmp[1:] }
     ret.update(_pilot)
-  print( json.dumps( ret ))
+  #print( json.dumps( ret ))
 
-with open(RESULT_DATA, newline='') as total_csv:
-  total_result = csv.reader(total_csv, delimiter=' ', quotechar='|')
-
-
-pilot = "A1":{
-        "name": "A1",
-        "heat1":{
-            "lap":[-1,-1,-1,-1],
-            "total":-1,
-            "unixtime":99999999
-            }
-        }
-
-
-
-
+with open(DATA_result_file) as total_json:
+    result_data = total_json.readlines()
+    x = "".join(result_data)
+    y = json.loads( x )
+    print( y )
 
 
