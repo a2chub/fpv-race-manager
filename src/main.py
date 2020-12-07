3a8fa37e5706a699487642347ac7357bd38fc673
a2c <a2cgle@gmail.com>
2020-12-07 20:28:09 +0900

    add py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+
+
+import os
+import csv
+import json
+
+DATA_PATH = '../data/data_csv/'
+DATA_lap_file = 'Latest-Result.csv'
+DATA_result_file = 'result_A.csv'
+
+LAP_DATA = os.path.join(DATA_PATH, DATA_lap_file)
+RESULT_DATA = os.path.join(DATA_PATH, DATA_result_file)
+
+
+with open(LAP_DATA, newline='') as csvfile:
+  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+  ret = {}
+  for row in spamreader:
+    _tmp = ', '.join(row).split(',')
+    _pilot = {_tmp[0]: _tmp[1:] }
+    ret.update(_pilot)
+  print( json.dumps( ret ))
+
+with open(RESULT_DATA, newline='') as total_csv:
+  total_result = csv.reader(total_csv, delimiter=' ', quotechar='|')
+
+
+
+
+
+
+
