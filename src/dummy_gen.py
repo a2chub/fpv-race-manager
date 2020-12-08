179032815dc0b945c87237460a39c4d72f2ff8eb
a2c <a2cgle@gmail.com>
2020-12-08 17:39:32 +0900

    ファイル名の固定化とヒートのランダム化

--- src/dummy_gen.py
+++ src/dummy_gen.py
@@ -2,12 +2,21 @@ import numpy as np
 import random
 import datetime
 
-all_pilots = ['atushi', 'Saqoosha', 't.oka', 'inoue', 'masap']
+all_pilots = ['atusi', 'Saqoosha', 't.oka', 'inoue', 'masap']
 num_heats = 4
 
 
-for heat in range(1, num_heats+1):
-    with open('../data/heat{}.csv'.format(heat), 'w') as f:
+def makeResultCSV(_num_heats=num_heats):
+    for heat in range(1, _num_heats+1):
+        getResultCsv(heat)
+
+def getResultCsv(heat=""):
+    if heat=="":
+        f_name = '../data/heat.csv'
+        heat = random.randint(1,4)
+    else:
+        f_name = '../data/heat{}.csv'.format(heat)
+    with open(f_name, 'w') as f:
         for p in np.random.choice(all_pilots, 3, replace=False):
             l1 = random.uniform(25.0, 35.0)
             l2 = random.uniform(25.0, 35.0)
@@ -20,3 +29,7 @@ for heat in range(1, num_heats+1):
             data.extend([timestamp, heat])
             f.write(','.join([str(d) for d in data]))
             f.write('\n')
+
+if __name__ == "__main__":
+    #makeResultCSV()
+    getResultCsv()
